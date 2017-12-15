53940f55a748c18cd66f4482a44b1f35e55142f9
burizae <aaa.1470.333@gmail.com>
2017-12-15 09:33:26 +0900

    first_commit

---
+++ app/src/main/java/vorooooone/vorooooone/VorooooonActivity.kt
@@ -0,0 +1,12 @@
+package vorooooone.vorooooone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VorooooonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vorooooon)
+    }
+}
