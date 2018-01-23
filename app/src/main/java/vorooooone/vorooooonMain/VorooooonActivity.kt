45beca1c1a83bc217aaffd6350e48b58f93df069
burizae <aaa.1470.333@gmail.com>
2018-01-23 14:22:11 +0900

    Delete unused processing

--- app/src/main/java/vorooooone/vorooooonMain/VorooooonActivity.kt
+++ app/src/main/java/vorooooone/vorooooonMain/VorooooonActivity.kt
@@ -6,22 +6,17 @@ import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.speech.RecognizerIntent
-import android.widget.Button
 import android.widget.TextView
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import vorooooone.vorooooone.R
 import vorooooone.felhr.usbserial.UsbSerialDevice
 import vorooooone.felhr.usbserial.UsbSerialInterface
-//import Kotlinx.android.synthetic.main.activity_vorooooone
 
 import java.nio.ByteBuffer
 import android.app.PendingIntent
-import android.media.Image
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.ImageView
-import java.util.*
 
 
 class VorooooonActivity : Activity() {
@@ -32,24 +27,10 @@ class VorooooonActivity : Activity() {
     }
 
     private var textView: TextView? = null
-    private var mTextView: TextView? = null
-
-    //private var buttonStart: ImageView? = null
+    private var connection_view: TextView? = null
 
     private var mUsbManager: UsbManager? = null
     private var mUsbDevice: UsbDevice? = null
-
-    /*
-    private var upButton: Button? =null
-    private var frontButton: Button? =null
-    private var l_turnButton: Button? =null
-    private var r_turnButton: Button? =null
-    private var leftButton: Button? =null
-    private var rightButton: Button? =null
-    private var downButton: Button? =null
-    private var rearButton: Button? =null
-    private var stopButton: Button? =null
-    */
 
     private var orderNumber: Int = 0
     private var lang: Int = 0
@@ -58,16 +39,13 @@ class VorooooonActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vorooooon)
 
-
-
-
         // 認識結果を表示させる
         textView = findViewById(R.id.text_view)
 
-        //
-        mTextView = findViewById(R.id.test_view)
+        //接続状態の表示
+        connection_view = findViewById(R.id.Connection_Status)
 
-        //ボタンの設定
+        //各ボタンの設定
         val buttonStart = findViewById<ImageView>(R.id.button_start)
         val upButton = findViewById<ImageView>(R.id.UpButton)
         val frontButton = findViewById<ImageView>(R.id.FrontButton)
@@ -156,7 +134,7 @@ class VorooooonActivity : Activity() {
         val deviceList = mUsbManager!!.deviceList
 
         if (deviceList == null || deviceList.isEmpty()) {
-            mTextView!!.text = "no device found"
+            connection_view!!.text = "no device found"
         } else {
             var string = ""
 
@@ -170,7 +148,7 @@ class VorooooonActivity : Activity() {
                     string += "\n"
                 }
             }
-            mTextView!!.text = string
+            connection_view!!.text = string
         }
     }
 
@@ -194,14 +172,7 @@ class VorooooonActivity : Activity() {
             val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
             if (lang == 0) {
-                /*// 英語
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH.toString())*/
-
-                // Off line mode
                 intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true)
-            /*} else if (lang == 1) {
-                // Off line mode
-                intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true)*/
             } else {
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
             }
@@ -214,9 +185,7 @@ class VorooooonActivity : Activity() {
             textView!!.text = "No Activity "
         }
     }
-
-
-
+    
     //命令一覧との照合
     private fun OrderCheck(checkstr: String): Boolean {
         var orderResult: Boolean = false
